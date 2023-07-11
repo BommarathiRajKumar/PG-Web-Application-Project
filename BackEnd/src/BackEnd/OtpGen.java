eb3b5805f31632932340ce90b6f5cab80ba861c3
Raj Kumar <rajbommarathi999@gmail.com>
2023-07-11 13:54:09 +0530

    signup,login logics in backend and data base connectivity completed

---
+++ BackEnd/src/BackEnd/OtpGen.java
@@ -0,0 +1,80 @@
+package BackEnd;
+
+import java.util.Random;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+
+
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.mysql.jdbc.Driver;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet("/otpGen")
+public class OtpGen extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		try {
+			
+			res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+			res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+			res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+			res.setHeader("Access-Control-Allow-Credentials", "true");
+
+			
+			
+			String mobileNumber=req.getParameter("mobileNumber");
+			
+			String url = "jdbc:mysql://localhost:3306/pg";
+			String userName = "root";
+			String pass = "123456789";
+			Driver d=new Driver();
+			DriverManager.registerDriver(d);
+			Connection con = DriverManager.getConnection(url,userName,pass);
+			
+			
+			String query = "select count(*) from users where mobileNumber = ?";
+		    PreparedStatement pStm = con.prepareStatement(query);
+		    pStm.setString(1, mobileNumber);
+		    ResultSet resultSet= pStm.executeQuery();
+		    resultSet.next();
+		    int count = resultSet.getInt(1);
+		    
+		    if(count<=0) {
+		    	int otp=generateOTP();
+		    	String insertOtpQuery="insert into otp (mobileNumber,otp) values(?,?)";
+		    	PreparedStatement pStm1 = con.prepareStatement(insertOtpQuery);
+		    	pStm1.setString(1, mobileNumber);
+		    	pStm1.setInt(2, otp);
+		    	pStm1.executeUpdate();
+		    	
+		    	res.setStatus(HttpServletResponse.SC_OK);
+		        PrintWriter writer = res.getWriter();
+		        writer.println(otp);
+		    }else {
+		    	res.setStatus(409);
+		    }
+		    con.close();
+		}catch(Exception e) {
+			res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+		
+	}
+	
+	private int generateOTP() {
+        Random random = new Random();
+        return 100000 + random.nextInt(900000);
+    }
+}
---
+++ BackEnd/src/BackEnd/OtpValidAndSignup.java
@@ -0,0 +1,68 @@
+package BackEnd;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+
+
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import com.mysql.jdbc.Driver;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet("/OtpValidAndSignup")
+public class OtpValidAndSignup extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		try {
+			res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+			res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+			res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+			res.setHeader("Access-Control-Allow-Credentials", "true");
+	        
+			String mobileNumber=req.getParameter("mobileNumber");
+			int userOtp=Integer.parseInt(req.getParameter("otp"));
+			
+			String url = "jdbc:mysql://localhost:3306/pg";
+			String userName = "root";
+			String pass = "123456789";
+			Driver d=new Driver();
+			DriverManager.registerDriver(d);
+			Connection con = DriverManager.getConnection(url,userName,pass);
+			
+		    PreparedStatement qPstm = con.prepareStatement("select otp from otp where mobileNumber="+mobileNumber);
+		    ResultSet resultSet= qPstm.executeQuery();
+		    resultSet.next();
+		    int dataBaseOpt = resultSet.getInt(1);
+		    
+		    if(dataBaseOpt==userOtp) {
+		    	String insertQuery="insert into users (mobileNumber,password,ownerName) values(?,?,?)";
+		    	PreparedStatement pStmt1=con.prepareStatement(insertQuery);
+		    	pStmt1.setString(1, mobileNumber);
+		    	pStmt1.setString(2, req.getParameter("password"));
+		    	pStmt1.setString(3, req.getParameter("ownerName"));
+		    	pStmt1.executeUpdate();
+		    	res.setStatus(HttpServletResponse.SC_OK);
+		    	
+		    	PreparedStatement delStm=con.prepareStatement("delete from otp where mobileNumber="+mobileNumber);
+		    	delStm.executeUpdate();
+		    }else {
+		    	res.setStatus(409);
+		    }
+		    con.close();
+		}catch(Exception e) {
+			res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+		
+	}
+	
+}
